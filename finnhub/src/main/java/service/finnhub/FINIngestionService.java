a134905135eb9bd24f6e247917171130880d8c74
Conor-O-Mahony <conor.omahony@ucdconnect.ie>
2024-12-20 21:23:34 +0000

    Created finnhub API scraper

--- finnhub/src/main/java/service/finnhub/FINIngestionService.java
+++ finnhub/src/main/java/service/finnhub/FINIngestionService.java
@@ -1,21 +1,28 @@
 package service.finnhub;
 
+import org.json.JSONObject;
 import service.core.AbstractAPIScraper;
 import service.core.Stock;
 
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
-import java.net.URL;
 
 public class FINIngestionService extends AbstractAPIScraper {
 
-    private String APIkey = "KEY";
-    private String ticker = "AAPL";
-    private String APIname = "finnhub";
-    private URL APIurl = new URL("https://finnhub.io/api/v1/quote?symbol=" + ticker + "&token=" + APIkey);
-    private int rate = 60;
-    private String kafkaServers = "localhost:9092"; //CHANGE
-    private String kafkaTopic = "stockData";
+    // Ingestion service for ticker
+    private static String ticker = "AAPL";
+
+    // Define the API url to poll
+    private static String APIkey = "KEY";
+    private static String APIname = "finnhub";
+    private static String APIurl = "https://finnhub.io/api/v1/quote?symbol=" + ticker + "&token=" + APIkey;
+
+    // Define the number of polls per minute
+    private static int rate = 60;
+
+    // Define kafka details
+    private static String kafkaServers = "localhost:9092"; //CHANGE
+    private static String kafkaTopic = "stockData";
 
     public FINIngestionService(String kafkaServers, String kafkaTopic, String APIurl, String APIname, int rate) throws URISyntaxException, MalformedURLException {
         super(kafkaServers, kafkaTopic, APIurl, APIname, rate);
@@ -23,6 +30,19 @@ public class FINIngestionService extends AbstractAPIScraper {
 
     @Override
     public Stock transformData(String rawData) {
-        return null;
+        JSONObject json = new JSONObject(rawData);
+        return new Stock(
+                ticker,
+                json.getDouble("t"),
+                json.getDouble("o"),
+                json.getDouble("h"),
+                json.getDouble("l"),
+                json.getDouble("c")
+        );
+    }
+
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException {
+        FINIngestionService service = new FINIngestionService(kafkaServers,kafkaTopic,APIurl,APIname,rate);
+        service.poll();
     }
 }
