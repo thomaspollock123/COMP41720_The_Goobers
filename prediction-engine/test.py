204bf01d2a4063e110f41913f25a4bde83aca87d
thomaspollock123 <thomas.pollock@ucdconnect.ie>
2024-12-21 00:50:06 +0000

    Local MongDB database integration.

--- prediction-engine/test.py
+++ prediction-engine/test.py
@@ -1,6 +1,7 @@
 #import kafka
 #from kafka import KafkaConsumer, KafkaProducer
 import pandas as pd
+import pprint
 import numpy
 import sklearn
 import joblib
@@ -8,6 +9,14 @@ from joblib import load
 import schedule
 import time
 import json
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client["apple_stock"]
+collection = db["1min_interval"]
+cl = collection.find().sort("_id", -1).limit(20)
+db_data = pd.DataFrame(data=cl).sort_values(by="timestamp", ascending=True).drop(columns=["_id"])
 
 model_file = "models/Logistic_Regression_Model_no_index.pkl"
 data_location = "data/prediction_engine_test_data_final.json"
@@ -47,22 +56,21 @@ def timestamp_feature_creation(input_dataframe):
     input_dataframe['year_2024'] = 1
     return input_dataframe
 
-def close_feature_creation(input_dataframe):
+def close_feature_creation(input_dataframe, hist_data):
     # Creates rolling average and price difference features based off "close" value
-    hist_data = pd.read_json(historical_data)
-    input_dataframe["close_diff"] = hist_data["close"][22:].diff()
-    input_dataframe["close_previous_diff"] = hist_data["close_diff"][23:].shift(1)
-    input_dataframe['rolling_average_log'] = hist_data['close_previous_diff'][18:].ewm(span=5, adjust=False).mean()
-    input_dataframe['rolling_average_3'] = hist_data['close_previous_diff'][18:].rolling(window=3).mean()
-    input_dataframe['rolling_average_5'] = hist_data['close_previous_diff'][18:].rolling(window=5).mean()
-    input_dataframe['rolling_average_10'] = hist_data['close_previous_diff'][12:].rolling(window=10).mean()
+    #hist_data = pd.read_json(historical_data)
+    input_dataframe["close_diff"] = hist_data["close"].diff()[0]
+    input_dataframe["close_previous_diff"] = hist_data["close_diff"].shift()[0]
+    input_dataframe['rolling_average_log'] = hist_data['close_previous_diff'].ewm(span=5, adjust=False).mean()[0]
+    input_dataframe['rolling_average_3'] = hist_data['close_previous_diff'].rolling(window=3).mean()[0]
+    input_dataframe['rolling_average_5'] = hist_data['close_previous_diff'].rolling(window=5).mean()[0]
+    input_dataframe['rolling_average_10'] = hist_data['close_previous_diff'].rolling(window=10).mean()[0]
     return input_dataframe
 
 def load_data(input_data, model, scaler):
     # Loads Kafka input data and returns predictions
-    #input_dataframe = pd.read_json(input_data)
     input_dataframe = timestamp_feature_creation(input_data)
-    input_dataframe = close_feature_creation(input_dataframe)
+    input_dataframe = close_feature_creation(input_dataframe, db_data)
     input_dataframe_scaled = scaler.transform(input_dataframe.drop(columns=["close", "close_diff", "timestamp", "year"]))
     predictions = model.predict(input_dataframe_scaled)
     input_dataframe["prediction"] = predictions
@@ -86,11 +94,12 @@ def test_run():
     if b and b[0] and b[1]:
         c = load_data(a, b[0], b[1])
         d = kafka_output(c)
-        return print(d)
+        return print(d["prediction"])
 
 #a = kafka_input(data_location)
 #b = model_load(model_file)
 #c = load_data(a, b[0], b[1])
+#print(c)
 #d = kafka_output(c)
 #e = test_run()
 #print(e["timestamp"]["492095"])
