7cf23dfc9656a4bb3871b2b68cf5f02be5a4fe2e
thomaspollock123 <thomas.pollock@ucdconnect.ie>
2025-01-02 17:46:54 +0000

    "update_database" method added to prediction engine for updating outdated database on startup.

--- prediction/app/main.py
+++ prediction/app/main.py
@@ -1,12 +1,20 @@
+import json
+import time
+from urllib.parse import quote_plus
+import pandas as pd
 import schedule
+from joblib import load
 from kafka import KafkaProducer, KafkaConsumer
 from kafka.errors import KafkaError
-import pandas as pd
-from joblib import load
-import json
 from pymongo import MongoClient
-from urllib.parse import quote_plus
-import time
+import datetime
+from datetime import timedelta
+import requests
+import io
+import numpy as np
+
+# Alpha Vantage API KEY
+av_key = quote_plus("K6QQP60C5DSXN8MU")
 
 # MongoDB Cluster = TheGooberCluster
 password = quote_plus("C0OZACjKnWMF4GQz")
@@ -15,9 +23,9 @@ client = MongoClient(uri)
 db = client["apple_stock"]
 collection = db["stock_data"]
 
-cursor = collection.find().sort("_id", -1).limit(20)
-test = pd.DataFrame(data=cursor).sort_values(by="timestamp", ascending=True).drop(columns=["_id"])
-print(test)
+#cursor = collection.find().sort("_id", -1).limit(20)
+#test = pd.DataFrame(data=cursor).sort_values(by="timestamp", ascending=True).drop(columns=["_id"])
+#print(test)
 
 model_file = "models/Logistic_Regression_Model_final.pkl"
 try:
@@ -28,6 +36,35 @@ except FileNotFoundError:
     print(f"File {model_file} not found")
 except Exception as e:
     print(f"Error: {e}")
+
+def update_database():
+    # On startup, checks if data from the last 5 days is available in database. If no, updates database to include last 5 days of stock data.
+    current_time = datetime.datetime.now()
+    target_time = current_time - timedelta(days=5)
+    database_time = collection.find({"timestamp": {"$gte": target_time, "$lt": current_time}}).sort("_id", -1).limit(1)
+    time_check = pd.DataFrame(data=database_time).sort_values(by="timestamp", ascending=True).drop(columns=["_id"])
+    if time_check["timestamp"].iloc[0].strftime("%Y:%m:%d") == current_time.strftime("%Y:%m:%d"):
+        print("Database up to date")
+        return None
+    else:
+        print("Database not up to date. Updating now.....")
+        # Code below working is dependent on Alpha Vantage API stock data for January being available. Will work if "2025-{month}" is changed to "2024-12" in url but only updates up to Dec 31st.
+        """month = current_time.strftime("%m")
+        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AAPL&interval=1min&outputsize=full&month=2025-{month}&apikey={av_key}&datatype=csv'
+        response = requests.get(url)
+        new_data = response.text
+        csv_data = pd.read_csv(io.StringIO(new_data))
+        csv_data["timestamp"] = pd.to_datetime(csv_data["timestamp"])
+        csv_data_td = csv_data[(csv_data["timestamp"] >= target_time)]
+        csv_data_td = csv_data_td.drop(columns=["volume"])
+        csv_data_td["ticker"] = "AAPL"
+        csv_data_td["APIname"] = "alpha_vantage"
+        csv_data_td["close_diff"] = csv_data_td["close"].diff().fillna(0)
+        csv_data_td["prediction"] = np.where(csv_data_td["close_diff"] >= 0, 1, 0)
+        csv_data_td1 = csv_data_td.sort_values(by="timestamp", ascending=True).reset_index(drop=True)
+        collection.delete_many({"timestamp": {"$gte": target_time, "$lt": current_time}})
+        update = csv_data_td1.to_dict(orient="records")
+        collection.insert_many(update)"""
 
 def kafka_input(data):
     # Takes transformed data from "ingestion-service" and converts to readable format for predictions
@@ -40,7 +77,29 @@ def load_data(input_data, model, scaler):
     input_dataframe = timestamp_feature_creation(input_data)
     cl_local = collection.find().sort("_id", -1).limit(20)
     db_data = pd.DataFrame(data=cl_local).sort_values(by="timestamp", ascending=True).drop(columns=["_id"])
+
+    if len(db_data) < 10:
+        print("Insufficient historical data!")
+        return None
+
     input_dataframe = close_feature_creation(input_dataframe, db_data)
+
+    if input_dataframe['close_previous_diff'].isna().any():
+        print("NaN values in close_previous_diff. Setting it to 0")
+        input_dataframe = input_dataframe.assign(close_previous_diff=input_dataframe['close_previous_diff'].fillna(0.0))
+
+    if input_dataframe['rolling_average_3'].isna().any():
+        print("NaN values in 3 day rolling avg. Imputting the close_previous_diff")
+        input_dataframe = input_dataframe.assign(rolling_average_3=input_dataframe['rolling_average_3'].fillna(input_dataframe['close_previous_diff']))
+
+    if input_dataframe['rolling_average_5'].isna().any():
+        print("NaN values in 5 day rolling avg. Imputting the 3 day rolling avg")
+        input_dataframe = input_dataframe.assign(rolling_average_5=input_dataframe['rolling_average_5'].fillna(input_dataframe['rolling_average_3']))
+
+    if input_dataframe['rolling_average_10'].isna().any():
+        print("NaN values in 10 day rolling avg. Imputting the 5 day rolling avg")
+        input_dataframe = input_dataframe.assign(rolling_average_10=input_dataframe['rolling_average_10'].fillna(input_dataframe['rolling_average_5']))
+
     model_order = ["ticker", "timestamp", "open", "high", "low", "close", "month", "day", "hour", "minute", "year_2022", "year_2023", "year_2024", "close_diff", "close_previous_diff", "rolling_average_log", "rolling_average_3", "rolling_average_5", "rolling_average_10", "year", "APIname"]
     input_dataframe = input_dataframe.reindex(columns=model_order)
     input_dataframe_scaled = scaler.transform(input_dataframe.drop(columns=["close_diff", "timestamp", "year", "APIname", "ticker"]))
@@ -63,8 +122,8 @@ def timestamp_feature_creation(input_dataframe):
     input_dataframe['minute'] = input_dataframe['timestamp'].dt.minute
     input_dataframe['year_2022'] = 0
     input_dataframe['year_2023'] = 0
-    input_dataframe['year_2024'] = 1
-    input_dataframe['year_2025'] = 0
+    input_dataframe['year_2024'] = 0
+    input_dataframe['year_2025'] = 1
     return input_dataframe
 
 def close_feature_creation(input_dataframe, hist_data):
@@ -123,6 +182,8 @@ def kafka_pipeline():
         print(f"Kafka error: {ke}")
     except Exception as e:
         print(f"Error in pipeline: {e}")
+
+update_database()
 
 schedule.every(5).seconds.do(kafka_pipeline)
 print("Starting prediction engine...")
